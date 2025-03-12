d1687e4f708323fc45e522656da4a10d55f90668
JcLobos16 <lobosjocelyn_bsit@plmun.edu.ph>
2025-03-12 18:59:37 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- create_bookstore_schema.sql
+++ create_bookstore_schema.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE `BookstoreDB`;
-USE `BookstoreDB`;
 -- Books Table [cite: 11]
 CREATE TABLE `Books` (
    `ISBN` VARCHAR(20) PRIMARY KEY,
    `Title` VARCHAR(255) NOT NULL,
    `Price` DECIMAL(10,2) NOT NULL
 );
+
 -- Authors Table [cite: 11]
 CREATE TABLE `Authors` (
    `AuthorID` INT PRIMARY KEY,
    `Name` VARCHAR(255) NOT NULL
 );
+
 -- BookAuthors Table (Associative Entity) [cite: 11]
 CREATE TABLE `BookAuthors` (
    `ISBN` VARCHAR(20),
@@ -19,12 +20,14 @@ CREATE TABLE `BookAuthors` (
    FOREIGN KEY (`ISBN`) REFERENCES `Books`(`ISBN`),
    FOREIGN KEY (`AuthorID`) REFERENCES `Authors`(`AuthorID`)
 );
+
 -- Customers Table [cite: 11]
 CREATE TABLE `Customers` (
    `CustomerID` INT PRIMARY KEY,
    `Name` VARCHAR(255) NOT NULL,
    `Email` VARCHAR(255) NOT NULL
 );
+
 -- Orders Table [cite: 11]
 CREATE TABLE `Orders` (
    `OrderID` INT PRIMARY KEY,
@@ -32,6 +35,7 @@ CREATE TABLE `Orders` (
    `OrderDate` DATE NOT NULL,
    FOREIGN KEY (`CustomerID`) REFERENCES `Customers`(`CustomerID`)
 );
+
 -- OrderDetails Table [cite: 11]
 CREATE TABLE `OrderDetails` (
    `OrderID` INT,
