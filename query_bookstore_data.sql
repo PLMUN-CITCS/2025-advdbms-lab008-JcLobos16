e0e50ed1a93e23fd14c9bcc48c95a346dc5bdd1e
JcLobos16 <lobosjocelyn_bsit@plmun.edu.ph>
2025-03-12 17:31:08 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- query_bookstore_data.sql
+++ query_bookstore_data.sql
@@ -1,4 +1,3 @@
-USE `BookstoreDB`;
 SELECT `c`.`Name` AS `CustomerName`, `o`.`OrderDate`, `b`.`Title` AS `BookTitle`, `od`.`Quantity`
 FROM `Customers` `c`
 JOIN `Orders` `o` ON `c`.`CustomerID` = `o`.`CustomerID`
