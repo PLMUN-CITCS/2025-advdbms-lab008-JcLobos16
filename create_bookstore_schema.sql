7d9bdd296012a2d77f49803990ad2cbf91471e64
JcLobos16 <lobosjocelyn_bsit@plmun.edu.ph>
2025-03-12 18:31:56 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- create_bookstore_schema.sql
+++ create_bookstore_schema.sql
@@ -6,13 +6,11 @@ CREATE TABLE `Books` (
    `Title` VARCHAR(255) NOT NULL,
    `Price` DECIMAL(10,2) NOT NULL
 );
-
 -- Authors Table [cite: 11]
 CREATE TABLE `Authors` (
    `AuthorID` INT PRIMARY KEY,
    `Name` VARCHAR(255) NOT NULL
 );
-
 -- BookAuthors Table (Associative Entity) [cite: 11]
 CREATE TABLE `BookAuthors` (
    `ISBN` VARCHAR(20),
@@ -21,14 +19,12 @@ CREATE TABLE `BookAuthors` (
    FOREIGN KEY (`ISBN`) REFERENCES `Books`(`ISBN`),
    FOREIGN KEY (`AuthorID`) REFERENCES `Authors`(`AuthorID`)
 );
-
 -- Customers Table [cite: 11]
 CREATE TABLE `Customers` (
    `CustomerID` INT PRIMARY KEY,
    `Name` VARCHAR(255) NOT NULL,
    `Email` VARCHAR(255) NOT NULL
 );
-
 -- Orders Table [cite: 11]
 CREATE TABLE `Orders` (
    `OrderID` INT PRIMARY KEY,
@@ -36,7 +32,6 @@ CREATE TABLE `Orders` (
    `OrderDate` DATE NOT NULL,
    FOREIGN KEY (`CustomerID`) REFERENCES `Customers`(`CustomerID`)
 );
-
 -- OrderDetails Table [cite: 11]
 CREATE TABLE `OrderDetails` (
    `OrderID` INT,
